d251b80290bed1bab2b7c08704ca38a9b55b18ad
Bilal-yat <59781894+Bilal-yat@users.noreply.github.com>
2024-04-16 00:03:38 -0400

    Update mysql_init.php

--- mysql_init.php
+++ mysql_init.php
@@ -1,8 +1,8 @@
 <?php
 // Connection Variables
 $host = "localhost";
-$username = "id22029861_root";
-$user_passw = "Gr3atPass.1";
+$username = "root";
+$user_passw = "";
 $database_used = "id22029861_hfests";
 
 //Connecting to mysql database
